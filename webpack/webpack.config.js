578194da6ce93750422bcab0e59d5781b70d93e3
Roman <romazukov1993@gmail.com>
2019-04-16 10:25:59 +0200

    Added HTML webpack plugin

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,17 @@
+var htmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: "./src/scripts/app.js",
     output: {
         filename: "./app.bundle.js"
-    }
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            hash: true,
+            title: 'My Awesome application',
+            myPageHeader: 'Hello World',
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
 };
