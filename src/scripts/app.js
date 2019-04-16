122848bde4c5a307145c3624ed2c7ff6da0a84fc
Roman <romazukov1993@gmail.com>
2019-04-16 13:50:39 +0200

    Added settings pages, bundled different chunks

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,11 @@
+import * as helperModule from './my-helper-module';
+import * as _ from 'lodash';
+
 console.log('Welcome! Greetings from app.js');
+console.log(helperModule.greetings);
+
+
+var arr=[ 1, 2, 3];
+_.each(arr,function(val) {
+    console.log('Output from Lodash _.each for Element ' + val);
+});
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,18 +1,30 @@
 var htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./src/scripts/app.js",
+    entry: {
+        app: './src/scripts/app.js',
+        settings: './src/scripts/settings.js'
+    },
     output: {
-        filename: "./app.bundle.js"
+        filename: "./[name].bundle.js"
     },
     watch: true,
     plugins: [
         new htmlWebpackPlugin({
             hash: true,
             title: 'My Awesome application',
-            myPageHeader: 'Hello World',
+            myPageHeader: 'Hello World!',
             template: './src/index.html',
+            chunks: ['app'],
             filename: './index.html'
+        }),
+        new htmlWebpackPlugin({
+            hash: true,
+            title: 'My Awesome application',
+            myPageHeader: 'Settings',
+            template: './src/index.html',
+            chunks: ['app', 'settings'],
+            filename: './settings.html'
         })
     ]
 };
