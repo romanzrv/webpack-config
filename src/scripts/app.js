3131f8fefff3b58746bdbc03611122e25dc8e0cd
Roman <romazukov1993@gmail.com>
2019-04-17 13:21:53 +0200

    Added SASS loader

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,12 +2,13 @@ import * as helperModule from './my-helper-module';
 import * as _ from 'lodash';
 import './sharedModule';
 import './../styles/app.css';
+import './../styles/appStyles.scss';
 
 console.log('Welcome! Greetings from app.js');
 console.log(helperModule.greetings);
 
 
-var arr=[ 1, 2, 3];
-_.each(arr,function(val) {
+var arr = [1, 2, 3];
+_.each(arr, function (val) {
     console.log('Output from Lodash _.each for Element ' + val);
 });
